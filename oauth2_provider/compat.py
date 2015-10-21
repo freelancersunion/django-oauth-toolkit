8bd3a3078928b916e251670a56cb91f7c594100e
Alex Zagorodniuk <typhoon.man@gmail.com>
2015-10-21 13:18:00 -0400

    use custom user model

--- oauth2_provider/compat.py
+++ oauth2_provider/compat.py
@@ -21,7 +21,7 @@ except ImportError:
 
 # Django 1.5 add support for custom auth user model
 if django.VERSION >= (1, 5):
-    AUTH_USER_MODEL = settings.AUTH_USER_MODEL
+    AUTH_USER_MODEL = settings.OAUTH2_PROVIDER_USER_MODEL
 else:
     AUTH_USER_MODEL = 'auth.User'
 
